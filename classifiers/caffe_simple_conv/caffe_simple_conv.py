a46b3dc6937bcb97196556be8081b5e198f26de0
Eran Shirazi <eranshirazi@gmail.com>
2017-02-10 14:07:53 +0200

    4 layer conv net + 2 fc layers, current performace 89% on test set

---
+++ classifiers/caffe_simple_conv/caffe_simple_conv.py
@@ -0,0 +1,71 @@
+import glob
+import random
+import re
+
+import numpy as np
+import os
+import caffe
+
+from util.paths import temp_path
+
+SCRIPT_PATH = os.path.dirname(__file__)
+
+caffe.set_mode_cpu()
+
+model_def = os.path.join(SCRIPT_PATH, 'train_val.prototxt')
+model_weights = os.path.join(SCRIPT_PATH, 'simple_train_iter_1000.caffemodel')
+
+net = caffe.Net(model_def,      # defines the structure of the model
+                model_weights,  # contains the trained weights
+                caffe.TEST)     # use test mode (e.g., don't perform dropout)
+
+# load the mean ImageNet image (as distributed with Caffe) for subtraction
+# create transformer for the input called 'data'
+transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
+
+transformer.set_transpose('data', (2, 0, 1))  # move image channels to outermost dimension
+# transformer.set_channel_swap('data', (2, 1, 0))
+# transformer.set_raw_scale('data', 255)      # rescale from [0, 1] to [0, 255]
+# transformer.set_mean('data', np.array([33]))            # subtract the dataset-mean value in each channel
+# transformer.set_mean('data', np.array([-330]))            # subtract the dataset-mean value in each channel
+
+LABEL_REGEX = re.compile("([0123456789]+)\\..*?")
+
+def test_path(path):
+    correct = 0
+    total = 0
+
+    for file_path in glob.glob(os.path.join(path, "*.png")):
+        image = caffe.io.load_image(file_path, color=False)
+        cur_correct = int(LABEL_REGEX.findall(os.path.basename(file_path))[0])
+
+        transformed_image = transformer.preprocess('data', image)
+        # print image.shape, transformed_image.shape
+
+        # print transformed_image
+        # break
+
+        # copy the image data into the memory allocated for the net
+        net.blobs['data'].data[...] = transformed_image
+
+        ### perform classification
+        output = net.forward()
+
+        # print output
+        cur_predicted = output["fc8"][0].argmax()
+
+        print os.path.basename(file_path) + " - " + str(cur_predicted)
+
+        total += 1
+        if cur_correct == cur_predicted:
+            correct += 1
+            # output_prob = output['fc8'][0]  # the output probability vector for the first image in the batch
+        # print output
+        # print 'predicted class is:', output_prob.argmax() + 1
+
+    print "Identified {} of {}, {:.2%}".format(correct, total, float(correct) / float(total))
+
+if __name__ == "__main__":
+    test_path(temp_path("vis"))
+    # test_path(os.path.join(SCRIPT_PATH, "my_samples"))
+    # print LABEL_REGEX.findall("/Users/eranshirazi/Home/Dev/cnn-classify-digits/util/../temp/vis/image8900-label9.png")
--- visualize.py
+++ visualize.py
@@ -1,12 +1,18 @@
 import os
+import random
 
 import PIL.Image, PIL.ImageOps
+import leveldb
+import caffe
+
+import lmdb
+import numpy
 
 from util.parse_data_files import read_image_file_header, read_label_file_header
 from util.paths import data_path, temp_path, mkdir_recursive
 
 
-def convert_to_image_files(image_batch_file, label_batch_file, skip_first_n_images=0, save_n_images=100):
+def convert_to_image_files(image_batch_file, label_batch_file, skip_first_n_images=0, save_n_images=100, step=1):
     image_batch_file = open(data_path(image_batch_file), "rb")
     image_file_header = read_image_file_header(image_batch_file)
     single_image_size = image_file_header["num_rows"] * image_file_header["num_columns"]
@@ -24,12 +30,84 @@ def convert_to_image_files(image_batch_file, label_batch_file, skip_first_n_imag
         label = ord(label_batch_file.read(1))
 
         image = PIL.Image.frombytes("L", (image_file_header["num_rows"], image_file_header["num_columns"]), image_data)
-        image = PIL.ImageOps.invert(image)
-        image.save(
-            open(temp_path("vis", "image{}-label{}.png".format(i, label)), "wb"),
-            "png"
-        )
+        # image = PIL.ImageOps.invert(image)
+        if i % step == 0:
+            image.save(
+                open(temp_path("vis", "image{}-label{}.png".format(i, label)), "wb"),
+                "png"
+            )
+
+
+def average_data_set(image_batch_file, sample_approx_n_images):
+    image_batch_file = open(data_path(image_batch_file), "rb")
+    image_file_header = read_image_file_header(image_batch_file)
+    single_image_size = image_file_header["num_rows"] * image_file_header["num_columns"]
+
+    total_images = image_file_header["num_items"]
+
+    keep_chance = max(min(float(sample_approx_n_images) / float(total_images), 1.0), 0.0)
+
+    images_to_avg = []
+
+    for i in xrange(total_images):
+        image_data = image_batch_file.read(single_image_size)
+
+        if random.random() <= keep_chance:
+            image = PIL.Image.frombytes("L", (image_file_header["num_rows"], image_file_header["num_columns"]), image_data)
+            images_to_avg.append(image)
+
+    avg = numpy.zeros((images_to_avg[0].size[0], images_to_avg[0].size[1]), numpy.float)
+    n = len(images_to_avg)
+
+    for image in images_to_avg:
+        imarr = numpy.array(image, dtype=numpy.float)
+        avg += imarr / n
+
+    return numpy.average(avg)
+
+
+def create_lmdb(lmdb_filename, image_batch_file, label_batch_file):
+    image_batch_file = open(data_path(image_batch_file), "rb")
+    image_file_header = read_image_file_header(image_batch_file)
+    single_image_size = image_file_header["num_rows"] * image_file_header["num_columns"]
+
+    label_batch_file = open(data_path(label_batch_file), "rb")
+    label_file_header = read_label_file_header(label_batch_file)
+
+    num_images = image_file_header["num_items"]
+
+    # We need to prepare the database for the size. We'll set it 10 times
+    # greater than what we theoretically need. There is little drawback to
+    # setting this too big. If you still run into problem after raising
+    # this, you might want to try saving fewer entries in a single
+    # transaction.
+    map_size = single_image_size * num_images * 10
+
+    env = lmdb.open(data_path(lmdb_filename), map_size=map_size)
+
+    size_x = image_file_header["num_columns"]
+    size_y = image_file_header["num_rows"]
+
+    with env.begin(write=True) as txn:
+        # txn is a Transaction object
+        for i in xrange(num_images):
+            print (i + 1), "/", num_images
+            image_data = image_batch_file.read(single_image_size)
+            label = ord(label_batch_file.read(1))
+
+            datum = caffe.proto.caffe_pb2.Datum()
+            datum.channels = 1
+            datum.height = size_y
+            datum.width = size_x
+            datum.data = image_data
+            datum.label = label
+            str_id = '{:08}'.format(i)
+
+            # The encode is only essential in Python 3
+            txn.put(str_id.encode('ascii'), datum.SerializeToString())
 
 
 if __name__ == "__main__":
-    convert_to_image_files("t10k-images-idx3-ubyte", "t10k-labels-idx1-ubyte")
+    convert_to_image_files("t10k-images-idx3-ubyte", "t10k-labels-idx1-ubyte", 0, 10000, 100)
+    # print average_data_set("train-images-idx3-ubyte", 1000)
+    # create_lmdb("train.lmdb", "train-images-idx3-ubyte", "train-labels-idx1-ubyte")
